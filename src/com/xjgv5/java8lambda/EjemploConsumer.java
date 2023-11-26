6301db2b5ea0997baca46a992b6e30c60f09fd2f
Javier Gomez <javier.gov@outlook.es>
2023-11-26 00:28:00 -0600

    Suppliers

--- src/com/xjgv5/java8lambda/EjemploConsumer.java
+++ src/com/xjgv5/java8lambda/EjemploConsumer.java
@@ -1,9 +1,14 @@
 package com.xjgv5.java8lambda;
 
+import com.xjgv5.java8lambda.models.Usuario;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
     public static void main(String[] args) {
@@ -15,11 +20,30 @@ public class EjemploConsumer {
 
         consumidor.accept(new Date());
 
-        BiConsumer<String, Integer> consumidorbi = (nombre,edad) -> {
-              //cuerpo de la funcion
-            System.out.println(nombre + " tiene " + edad);
-        };
+        BiConsumer<String, Integer> consumidorbi = (nombre,edad) -> System.out.println(nombre + " tiene " + edad);
+
 
         consumidorbi.accept("Andres", 23);
+
+        Consumer<String> consumidor2 = System.out::println;
+        consumidor2.accept("hola mundo labda");
+
+        List<String> nombre = Arrays.asList("andres", "pepe", "luz", "paco");
+        nombre.forEach(consumidor2);
+
+        Supplier<Usuario> crearUsuario = Usuario::new;
+        Usuario usuario = new Usuario();
+
+        BiConsumer<Usuario, String> asignarNombre =Usuario::setNombre;
+
+
+
+        asignarNombre.accept(usuario, "Andres");
+
+        System.out.println("Nombre usuario : " + usuario.getNombre());
+
+        Supplier<String> proveedor = () -> "hola mundo supplier";
+
+        System.out.println(proveedor.get());
     }
 }
---
+++ src/com/xjgv5/java8lambda/models/Usuario.java
@@ -0,0 +1,13 @@
+package com.xjgv5.java8lambda.models;
+
+public class Usuario {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
